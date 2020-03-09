21ceddd779598735cd37b3722a5054a6c467e181
camberbridge <camberbridgecircuit@gmail.com>
2020-03-09 14:09:11 +0900

    Added a priority flag.

--- marathon/udp_reciever.py
+++ marathon/udp_reciever.py
@@ -23,23 +23,24 @@ def get_place(distance):
     if row[3] >= distance:
       print(row[3], distance, row[1].decode("utf-8"))
       return row[1].decode("utf-8"), "landmark_en"
-  return "AAAAA_PJ", "aaaaaaa_pe"
+  return "日　比　谷", "aaaaaaa_pe"
 
 def str_parser(msg):
   if len(msg) != 73:
     return None
   v_type = msg[11]
   v_num = str(int(msg[12:14]))
-  distance = str(int(msg[20:23]))+"."+msg[23:26]
+  distance = str(int(msg[20:23]))+"."+msg[23:25]
   speed = str(int(msg[48:51]))+"."+msg[51:52]
-  return v_type, v_num, distance, speed
+  yusen = str(msg[7])
+  return v_type, v_num, distance, speed, yusen
 
 while True:
   try:
     msg, address = s.recvfrom(8192)
     data = str_parser(msg)
 
-    if data is not None:
+    if data is not None and data[4] == "1":
       print(data)
       pj, pe = get_place(data[2])
       cmd = "./distributor.sh " + data[0] + " " + data[1] + " " + data[2] + " " + data[3] + " '" + pj + "' " + pe
