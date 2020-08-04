1f4de745f6ec470868ece66f7a3365f34736c7c0
camberbridge <camberbridgecircuit@gmail.com>
2020-08-04 13:07:09 +0900

    fix: Fixed bug that is not send full data on serial

--- marathon/serial/serial_comm.py
+++ marathon/serial/serial_comm.py
@@ -1,6 +1,7 @@
 import serial
 import serial.tools.list_ports
 import sys
+import time
 baudrate = 9600
 
 # Send sp_read (that is ball speed).
@@ -12,15 +13,16 @@ def sender(sp_read):
 
     # Initialize serial.
     port = serial.tools.list_ports.comports()[-1].device
-    ser = serial.Serial(port, baudrate, parity = serial.PARITY_NONE)
-    # Prepare data.
-    sp_str = "00"+str(sp_read)
-    data = sp_str[-3]+sp_str[-2]+sp_str[-1]+'\x20\x20\x20\x0d'
-    data_a = data.encode('ascii')
+    with serial.Serial(port, baudrate, parity = serial.PARITY_NONE) as ser:
+      # Prepare data.
+      sp_str = "00"+str(sp_read)
+      data = sp_str[-3]+sp_str[-2]+sp_str[-1]+'\x20\x20\x20\x0d'
+      data_a = data.encode('ascii')
 
-    ser.write(data_a)
-    ser.close()
-    print(data_a)
+      ser.write(data_a)
+      # Refer: https://stackoverflow.com/questions/38950613/have-to-use-sleep-with-pyserial-when-opening-com-port-for-arduino-nano
+      time.sleep(2)
+      print(data_a)
   else:
     print("ERR: input is not integer.")
 
